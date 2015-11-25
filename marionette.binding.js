b8e2b918a9b62eb0d15af209da77874fd8437f43
Joe Simpson <headbangerkenny@gmail.com>
2015-11-25 15:06:08 +0000

    ES6-ify
    - Include Bable to compile to ES6
    - Add some classes
    - Add some ES6!
    - Move functionality out of horrid long block

--- marionette.binding.js
+++ marionette.binding.js
@@ -1,12 +1,141 @@
-var MarionetteBinding = {};
+/**
+Marionette Binding
+
+Copyright 2015 Pebble
+*/
+let Marionette = null;
 
 if(window['Backbone'] && window['Marionette']){
-  var Marionette = Backbone.Marionette;
+  Marionette = Backbone.Marionette;
 } else{
-  var Marionette = require('backbone.marionette');
+  Marionette = require('backbone.marionette');
 }
 
-MarionetteBinding.BindingMixin = {
+/*
+This looks after a value to monitor on the model. This allows us to do
+some neat things such as "value__isnull" a la Django style
+*/
+export class ModelValue {
+  constructor(model, key){
+    this.model = model;
+    this.key = key;
+  }
+
+  get() {
+    return this.model.get(this.key);
+  }
+
+  set(val, args) {
+    return this.model.set(this.key, val, args);
+  }
+
+  change(fn){
+    this.model.on("change:" + this.key, fn);
+  }
+}
+
+/*
+This deals with the binding and keeping it all up to date
+
+@abstract
+*/
+export class Binding {
+  constructor(element, val, jQ){
+    this.element = element;
+    this.val = val;
+    this.$ = jQ;
+  }
+
+  start(){
+    throw new Error("start() not implemented");
+  }
+}
+
+export class ValueBinding extends Binding {
+  start(){
+    // Initial Value
+    this.element.val(this.val.get());
+
+    // Update
+    var eventHandler = () => {
+      this.val.set(this.element.val(), {_sender: this.element});
+    };
+    this.element.on("keyup", eventHandler).on(
+      "change", eventHandler).on("__updated", eventHandler);
+
+    // Listen to changes
+    this.val.change((model, value, options) => {
+      if(options['_sender'] == this.element) return; // Don't loop!
+      this.element.val(this.val.get());
+    });
+  }
+}
+
+export class TextBinding extends Binding {
+  start(){
+    // Initial Value
+    this.element.text(this.val.get());
+
+    // Listen to changes
+    this.val.change((model, value) => {
+      this.element.text(this.val.get());
+    });
+  }
+}
+
+export class HtmlBinding extends Binding {
+  start(){
+    // Initial Value
+    this.element.html(this.val.get());
+
+    // Listen to changes
+    this.val.change((model, value) => {
+      this.element.html(this.val.get());
+    });
+  }
+}
+
+export class CheckedBinding extends Binding{
+  start(){
+    // Update
+    this.element.on("click", (e) => {
+      var tick = this.$(e.target);
+      var type = tick.attr("type");
+      if(type == "radio"){
+        this.val.set(tick.attr('value'), {_sender: el});
+      } else if(type == "checkbox"){
+        this.val.set(tick.is(':checked'), {_sender: el});
+      }
+    });
+
+    // Listen to changes
+    this.val.change(this.change);
+    this.change(null, this.val.get(), {});
+  }
+
+  change(model, value, options){
+    if(options['_sender'] == this.element) return; // Don't loop!
+    this.element.each((k, v) => {
+      var type = this.$(v).attr("type");
+      if(type == "checkbox"){
+        this.$(v).prop("checked", value);
+      } else if(type == "radio"){
+        if(this.$(v).attr("value") == value){
+          this.$(v).get(0).checked = true;
+        }
+      }
+    });
+  }
+}
+
+export let Bindings = {
+  'value': ValueBinding,
+  'text': TextBinding,
+  'html': HtmlBinding,
+  'checked': CheckedBinding,
+};
+
+export let BindingMixin = {
   bindings: {},
   onRender: function(){
     this.startBindings();
@@ -28,14 +157,14 @@ MarionetteBinding.BindingMixin = {
     var self = this;
     _.each(bindings, function(what, bind_to){
       // Split up the definition
-      var binding = bind_to.split(" ");
+      let binding = bind_to.split(" ");
       if(binding.length != 2){
         throw new Error("Binding not setup correctly");
       }
 
       // Deal with the binding definition
-      var type = binding[0];
-      var el = binding[1];
+      let type = binding[0];
+      let el = binding[1];
       // @ui.etc is dealt with
       if(el.indexOf("@ui.") == 0){
         el = this.ui[el.substr(4)];
@@ -48,73 +177,17 @@ MarionetteBinding.BindingMixin = {
         el = this.$(el);
       }
 
-      // Now we change based on what we asked to do
-      if(type == "value" || type == "val"){
-        // Initial Value
-        el.val(function(){return model.get(what)});
+      let val = new ModelValue(model, what);
 
-        // Update
-        var eventHandler = function(){
-          model.set(what, el.val(), {_sender: el});
-        };
-        el.on("keyup", eventHandler).on("change", eventHandler).on("__updated", eventHandler);
-
-        // Listen to changes
-        model.on("change:" + what, function(model, value, options){
-          if(options['_sender'] == el) return; // Don't loop!
-          el.val(model.get(what));
-        });
-      } else if(type == "text"){
-        // Initial Value
-        el.text(model.get(what));
-
-        // Listen to changes
-        model.on("change:" + what, function(model, value){
-          el.text(model.get(what));
-        });
-      } else if(type == "html"){
-        // Initial Value
-        el.html(model.get(what));
-
-        // Listen to changes
-        model.on("change:" + what, function(model, value){
-          el.html(model.get(what));
-        });
-      } else if(type == "checked"){
-        // Update
-        el.on("click", function(e){
-          var tick = self.$(e.target);
-          var type = tick.attr("type");
-          if(type == "radio"){
-            model.set(what, tick.attr('value'), {_sender: el});
-          } else if(type == "checkbox"){
-            model.set(what, tick.is(':checked'), {_sender: el});
-          }
-        });
-
-        // Listen to changes
-        model.on("change:" + what, function(model, value, options){
-          if(options['_sender'] == el) return; // Don't loop!
-          el.each(function(){
-            var type = self.$(this).attr("type");
-            if(type == "checkbox"){
-              self.$(this).prop("checked", value);
-            } else if(type == "radio"){
-              if(self.$(this).attr("value") == value){
-                self.$(this).get(0).checked = true;
-              }
-            }
-          });
-        }).trigger("change:" + what, model, model.get(what), {});
+      if(Bindings[type]){
+        let binding = new Bindings[type](el, val, _.bind(this.$, this));
+        binding.start();
       } else{
-        throw new Error("Binding type is not recognised")
+        throw new Error("Binding not found for " + type);
       }
     }, this);
   }
 };
 
-MarionetteBinding.BindedView = Marionette.LayoutView.extend(
-  MarionetteBinding.BindingMixin);
-
-
-module.exports = MarionetteBinding;
+export let BindedView = Marionette.LayoutView.extend(
+  BindingMixin);
